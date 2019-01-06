fe57e7f66c41f19e798ba1b2a2316d02d45b0056
Bayu Abi Pamungkas <bayuabi31@gmail.com>
2019-01-06 10:32:30 +0800

    Update xilink_adc_tb.vhd

--- xilink_adc_tb.vhd
+++ xilink_adc_tb.vhd
@@ -1,68 +1,89 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
  
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
 ENTITY xilink_adc_tb IS
 END xilink_adc_tb;
  
 ARCHITECTURE behavior OF xilink_adc_tb IS 
-	 COMPONENT spiMaster
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT spiMaster
     PORT(
+         enable_spi : IN  std_logic;
+         clk_50Mhz : IN  std_logic;
+         clk_10Mhz : INOUT  std_logic;
          cs : OUT  std_logic;
          din : OUT  std_logic;
          dout : IN  std_logic;
-         iClk : IN  std_logic;
-         oClk : OUT  std_logic;
-         adcData : OUT  std_logic_vector(11 downto 0)
+         adc_data : OUT  std_logic_vector(11 downto 0);
+			adc_data_tempo : OUT std_logic_vector(11 downto 0);
+			count : OUT integer range 0 to 12
         );
     END COMPONENT;
     
 
    --Inputs
+   signal enable_spi : std_logic := '0';
+   signal clk_50Mhz : std_logic := '0';
    signal dout : std_logic := '0';
-   signal iClk : std_logic := '0';
 
-   --Outputs
+ 	--Outputs
+   signal clk_10Mhz : std_logic;
    signal cs : std_logic;
    signal din : std_logic;
-   signal oClk : std_logic;
-   signal adcData : std_logic_vector(11 downto 0);
+   signal adc_data : std_logic_vector(11 downto 0);
+	signal adc_data_tempo : std_logic_vector(11 downto 0);
+	signal count : integer range 0 to 12;
 
    -- Clock period definitions
-   constant iClk_period : time := 20 ns;
+   constant clk_50Mhz_period : time := 20 ns;
+   constant clk_10Mhz_period : time := 200 ns;
  
 BEGIN
  
 	-- Instantiate the Unit Under Test (UUT)
    uut: spiMaster PORT MAP (
+          enable_spi => enable_spi,
+          clk_50Mhz => clk_50Mhz,
+          clk_10Mhz => clk_10Mhz,
           cs => cs,
           din => din,
           dout => dout,
-          iClk => iClk,
-          oClk => oClk,
-          adcData => adcData
+          adc_data => adc_data,
+			 adc_data_tempo => adc_data_tempo,
+			 count => count
         );
 
    -- Clock process definitions
-   iClk_process :process
+   clk_50Mhz_process :process
    begin
-		iClk <= '1';
-		wait for iClk_period/2;
-		iClk <= '0';
-		wait for iClk_period/2;
+		clk_50Mhz <= '0';
+		wait for clk_50Mhz_period/2;
+		clk_50Mhz <= '1';
+		wait for clk_50Mhz_period/2;
    end process;
-	process
-	begin
-		wait for 1100 ns; dout <= '1';
-		wait for 200 ns; dout <= '0'; 
-		wait for 200 ns; dout <= '0';
-		wait for 200 ns; dout <= '0';
-		wait for 200 ns; dout <= '0';
+ 
+   -- Stimulus process
+   stim_proc: process
+   begin		
+		wait for 1400 ns; dout <= '1';
 		wait for 200 ns; dout <= '0';
 		wait for 200 ns; dout <= '1';
 		wait for 200 ns; dout <= '0';
-		wait for 200 ns; dout <= '0';
+		wait for 200 ns; dout <= '1';
 		wait for 200 ns; dout <= '0';
 		wait for 200 ns; dout <= '1';
+		wait for 200 ns; dout <= '0';
 		wait for 200 ns; dout <= '1';
-	end process;
+		wait for 200 ns; dout <= '0';
+		wait for 200 ns; dout <= '1';
+		wait for 200 ns; dout <= '0';
+		
+   end process;
+
 END;
